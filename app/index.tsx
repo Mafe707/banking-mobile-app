b9e5b857ec13dfdf81190577148f1b5f0d433e10
Mafe707 <fernanditarevelo@gmail.com>
2025-11-07 12:48:52 -0500

    Create mobile

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Here I am going to develop my app</Text>
+    </View>
+  );
+}
